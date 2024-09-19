904bcd9c652ef16179d54b07faeaa7c66e65adaf
dwjrgal <dwjrgl651@gmail.com>
2024-09-19 12:43:00 +0800

    auth route and controller

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,14 +2,21 @@ import express,{ Request, Response } from "express";
 
 import dotenv from "dotenv";
 
+import authRoute from "./routes/auth-route"
+
 dotenv.config();
 
 const PORT = process.env.PORT
 
+//express s  application object uusgej avav
 const app = express();
+app.use("api/v1/auth", authRoute)
+
+//middlewares
 
 app.get("/",(req: Request, res: Response) => {
     res.send("Welcome e-commerce API server")
 });
 
+//server asaah 
 app.listen(PORT, () => {console.log(`server running at localhost:${PORT}`)})
