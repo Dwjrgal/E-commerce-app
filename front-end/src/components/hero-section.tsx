4a2134eabeebc61dda287675a2358111198e07ff
dwjrgal <dwjrgl651@gmail.com>
2024-09-19 21:43:37 +0800

    slider img

---
+++ front-end/src/components/hero-section.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+  export const HeroSection = () => {
+  return (
+    <div>
+        <img src="/img/Logo/slider_img.png" alt="" />
+    </div>
+  )
+}
+
+
+ export const CategorySection = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
