76cc0226840a0511b136a2929c22256d20193854
dwjrgal <dwjrgl651@gmail.com>
2024-09-22 20:38:17 +0800

    login

--- back-end/src/controllers/auth-controller.ts
+++ back-end/src/controllers/auth-controller.ts
@@ -16,6 +16,19 @@ export const signup = async ( req:Request, res:Response) => {
    } 
 }
 
-export const login = ( req:Request, res:Response) => {
-    res.status(200).json( {message: "Login success"})
+export const login = async( req:Request, res:Response) => {
+   try {
+      const { email, password } = req.body;
+
+
+   if( !email  || !password) {
+      res.status(400).json({message: "Must not be null"})
+   }
+   const loggedUser = await User.create ({ email, password})
+
+   res.status(200).json( {message: "Login success", user: loggedUser})
+   } catch (error) {
+      res.status(201).json({ message: "server error", error: error})
+   }
+    
 }
--- front-end/src/app/(auth)/login/page.tsx
+++ front-end/src/app/(auth)/login/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useState } from 'react'
 
 const Login = () => {
+  const router = useRouter();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: ""
+  })
+
+
+
+
   return (
     <>
     <section className='flex flex-col items-center py-80 bg-slate-100'>
         <h2 className='font-bold text-lg mb-5'>Нэвтрэх</h2>
         <div className='flex flex-col gap-3'>
-        <input type="text" className='w-60 h-8 border rounded-full pl-2 text-xs' placeholder='Нэр'/>
-        <input type="text" className='w-60 h-8 border rounded-full pl-2 text-xs' placeholder='Имэйл хаяг' />
+        <input type="text" className='w-60 h-8 border rounded-full pl-2 text-xs' placeholder='Имэйл хаяг'/>
+        <input type="text" className='w-60 h-8 border rounded-full pl-2 text-xs' placeholder='Нууц үг' />
         </div>
         <div className='flex flex-col gap-4 mt-4'>
     <button className='w-60 h-8 border rounded-full bg-blue-600 text-white'>Үүсгэх</button>
