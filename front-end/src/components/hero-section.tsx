fdd0c7ad1097dfc5c8ff0b82496288b7278b3833
dwjrgal <dwjrgl651@gmail.com>
2024-09-27 13:07:19 +0800

    error

--- front-end/src/components/hero-section.tsx
+++ front-end/src/components/hero-section.tsx
@@ -1,41 +1,39 @@
-import React from 'react'
+import React from "react";
 
-  export const HeroSection = () => {
+export const HeroSection = () => {
   return (
     <div>
-        <img src="/img/Logo/slider_img.png" alt=""  className='w-full h-[500px]'/>
-        <CategorySection/>
+      <img src="/img/Logo/slider_img.png" alt="" className="w-full h-[500px]" />
+      <CategorySection />
     </div>
-  )
-}
+  );
+};
 
-
- export const CategorySection = () => {
+export const CategorySection = () => {
   return (
     <>
-    <section className='flex justify-center gap-3 mt-4'>
-      <div className='flex flex-col'>
-        <img src="" alt="" className='w-[250px] h-[390px] border rounded'/>
-        <h4 className='font-normal text-primary'> The Prompt Magazine</h4>
-        <p className='font-bold'> 120,000</p>
-      </div>
-      <div className='flex flex-col'>
-        <img src="" alt="" className='w-[250px] h-[390px] border rounded'/>
-        <h4 className='font-normal text-primary'> The Prompt Magazine</h4>
-        <p className='font-bold'> 120,000</p>
-      </div>
-      <div className='flex flex-col'>
-        <img src="" alt="" className='w-[250px] h-[390px] border rounded'/>
-        <h4 className='font-normal text-primary'> The Prompt Magazine</h4>
-        <p className='font-bold'> 120,000</p>
-      </div>
-      <div className='flex flex-col'>
-        <img src="" alt="" className='w-[250px] h-[390px] border rounded'/>
-        <h4 className='font-normal text-primary'> The Prompt Magazine</h4>
-        <p className='font-bold'> 120,000</p>
-      </div>
-    </section>
+      <section className="flex justify-center gap-3 mt-4">
+        <div className="flex flex-col">
+          <img src="" alt="" className="w-[250px] h-[300px] border rounded" />
+          <h4 className="font-normal text-primary"> The Prompt Magazine</h4>
+          <p className="font-bold"> 120,000</p>
+        </div>
+        <div className="flex flex-col">
+          <img src="" alt="" className="w-[250px] h-[300px] border rounded" />
+          <h4 className="font-normal text-primary"> The Prompt Magazine</h4>
+          <p className="font-bold"> 120,000</p>
+        </div>
+        <div className="flex flex-col">
+          <img src="" alt="" className="w-[250px] h-[300px] border rounded" />
+          <h4 className="font-normal text-primary"> The Prompt Magazine</h4>
+          <p className="font-bold"> 120,000</p>
+        </div>
+        <div className="flex flex-col">
+          <img src="" alt="" className="w-[250px] h-[300px] border rounded" />
+          <h4 className="font-normal text-primary"> The Prompt Magazine</h4>
+          <p className="font-bold"> 120,000</p>
+        </div>
+      </section>
     </>
-  )
-}
-
+  );
+};
