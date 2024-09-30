d4a15fcdd9e0fffd2b8cf39fa6e77e6a462bebe7
dwjrgal <dwjrgl651@gmail.com>
2024-09-30 21:18:06 +0800

    idk

--- front-end/src/app/loader/page.tsx
+++ front-end/src/app/loader/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "@/app/loader/loader-module.css";
+import "../globals.css";
 
 const Loader = () => {
   return (
-    <div className="h-[800px] flex justify-center items-center mx-auto ">
-      <span className="loader"></span>
+    <div className="h-[800px] flex justify-center items-center mx-auto gap-6 bg-slate-100">
+     <span className="loader"></span>
       <p className="text-xs">Түр хүлээнэ үү...</p>
     </div>
   );
