d8bad5cbcac64a3a2bbcf51c81e22dde635d7aad
Dwjrgal <dwjrgl651@gmail.com>
2024-11-15 10:24:22 +0800

    made changes

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
- export const connectDB = async ( uri: string) => {
-    try {
-          const con = await mongoose.connect(uri);
-          console.log ("Database connected", con.connection.host);
+export const connectDB = async (uri: string) => {
+  console.log("uri", uri);
+  try {
+    const con = await mongoose.connect(uri);
+    console.log("Database connected", con.connection.host);
 
-        console.log ("Database connected");
-    } catch (error) {
-        console.log ("Database cannot connect")
-    }
-
-}
-
-
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database cannot connect");
+  }
+};
