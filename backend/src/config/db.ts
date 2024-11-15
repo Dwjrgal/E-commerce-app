0671a9538b2831f6c4e77864d094cb42943ba77b
Dwjrgal <dwjrgl651@gmail.com>
2024-11-15 10:28:31 +0800

    backend deploy

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (uri: string) => {
-  console.log("uri", uri);
   try {
     const con = await mongoose.connect(uri);
     console.log("Database connected", con.connection.host);
